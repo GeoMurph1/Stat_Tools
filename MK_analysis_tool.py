3d947df9f6978468560d5031df35178bb9a509a8
Michael Murphy <37006534+GeoMurph1@users.noreply.github.com>
2020-05-18 15:43:21 -0700

    Updated user inputs; deleted extraneous code
    deleted unused code at end of script

--- MK_analysis_tool.py
+++ MK_analysis_tool.py
@@ -22,9 +22,9 @@ import pymannkendall as mk
 ### User-modified variables:
 ###############################################################################
 # Update version suffix as needed: 
-ver = "_Ricoh_01a"
+ver = "version"
 # Update filename as needed:
-f1 =  "LChem_Mann-Kendall_Query.xlsx"
+f1 =  "flatfile"
 ###############################################################################
 # Call today:
 today1 = str(dt.date.today())
@@ -122,73 +122,3 @@ df_rpt.to_excel(writer, 'MK_trends')
 writer.save()            
 os.chdir(cwd1)       
 # TODO: Add plots            
-"""
-analyte_list = ["Barium", "Chromium", "Chromium VI", "Mercury", "Zinc", "Vanadium"]
-color_list = ["blue", "orange", "red", "gray", "purple", "green"]
-df3 = df2 #.loc[df2.analyte.isin(analyte_list)]
-analyte_list1 = df3.analyte.unique().tolist()
-df3["max_conc"] = 0
-for a in analyte_list1:
-    df3.loc[df3.analyte == a, 'max_conc'] = df3.loc[df3.analyte == a].res_num.max()
-df3["res_norm"] = df3.res_num / df3.max_conc
-df3["date_ordinal"] = df3.date_dt.map(dt.datetime.toordinal)
-#g1 = sns.FacetGrid(df3, row="Well_ID1", col="analyte", hue ="analyte", height=4)
-#g1.map(plt.scatter, "date_dt", "res_norm")
-
-
-#g2 = sns.lmplot("date_ordinal", "res_norm", row="Well_ID1", col="analyte", data=df3, hue ="analyte", height=4)
-#g2.map(plt.scatter, "date_dt", "res_norm")
-df4 = df3[['Well_ID1', 'Date', 'date_dt', 'date_ordinal', 'qtr_ID', 'sample_ID',
-       'analyte', 'result', 'res_num', 'flag', 'd_nd', 'res_rand',
-       'rand_multiplier', 'max_conc', 'res_norm']]
-#df_Cr = df4.loc[df4.analyte == "Chromium"]
-
-
-ver = '_01d'
-today1 = str(dt.date.today())
-new_path = "EDA_plots_" + today1 + ver
-if os.path.exists(new_path) == False:
-    os.mkdir(new_path)
-cwd1 = os.getcwd()
-os.chdir(new_path)
-for l in loc_list:
-    df4_loc = df4.loc[df4.Well_ID1 == l]
-    df_gwe = df1_gwe.loc[df1_gwe.well_id == l].sort_values(by=["date_dt"])
-    title2 = "GWE vs. Time in " + l
-    for a, c in zip(analyte_list, color_list):
-        df_metal = df4_loc.loc[df4_loc.analyte == a].sort_values(by=["date_dt"])
-        title1 = a + ", "
-        ylabel = a + " mg/L"
-        metal = ColumnDataSource(df_metal)
-        gwe = ColumnDataSource(df_gwe)
-        mk_trend = mk.seasonal_test(df_metal.res_rand)
-        trend1 = mk_trend.trend
-        trend2 = mk_trend.p
-        trend_rpt = Label(x=50, y=50, x_units='screen', y_units='screen',
-                 text="Predicted M-K Trend (seasonal): " + trend1 +"; Calculated p-value: " + str(trend2), render_mode='css',
-                 border_line_color='black', border_line_alpha=1.0,
-                 background_fill_color='white', background_fill_alpha=0.5)
-        p1 = figure(width=1000, height=775, title = title1 + title2, x_axis_type="datetime", \
-                                y_axis_label= ylabel, x_axis_label= "Sample Date")
-        p1.y_range = Range1d(df_metal.res_num.min()-1, df_metal.res_num.max()+1)
-        p1.asterisk(df_metal.date_dt, df_metal.res_rand, color=c, size=13, legend_label = a)    
-        p1.extra_y_ranges = {"rng_gwe": Range1d(start = df_gwe.gwe_ft_amsl.min()-1, end = df_gwe.gwe_ft_amsl.max()+1 )}
-        p1.add_layout(LinearAxis(y_range_name="rng_gwe", axis_label='GWE ft amsl'), 'right')
-        p1.line(df_gwe.date_dt, df_gwe.gwe_ft_amsl, y_range_name='rng_gwe', legend_label= "GWE")
-        p1.inverted_triangle(df_gwe.date_dt, df_gwe.gwe_ft_amsl, y_range_name='rng_gwe', legend_label= "GWE")
-        p1.add_layout(trend_rpt)
-        output_file("trend_plot_"+ l + "_"+ a + "_gwe" + ".html")
-        show(p1)
-os.chdir(cwd1)        
-        
-        
-
-# Generate bokeh plots:
-
-
-writer = pd.ExcelWriter('Table_6_total_metals_long_fmt_processed_' + today1 + ver +'.xlsx')
-df4.to_excel(writer, 'Data')
-writer.save()
-
-#plt.show()
-"""
